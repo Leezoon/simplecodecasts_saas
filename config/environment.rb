3d825482e0d6b2a48c4cdf43a66f012b3ffb3694
Leezoon Khatua <leekhatua@gmail.com>
2016-02-25 22:14:41 +0000

    added mailgun addon and mailgun smtp setting

--- config/environment.rb
+++ config/environment.rb
@@ -5,11 +5,11 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-    :address => 'smtp.sendrid.net',
-    :port => '587',
-    :authentication => :plain,
-    :user_name =>ENV['SENDGRID_USERNAME'],
-    :password =>ENV['SENDGRID_PASSWORD'],
-    :domain => 'heroku.com',
-    :enable_startstls_auto => true
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'http://secret-atoll-90009.herokuapp.com/',
+  :authentication => :plain,
 }
+ActionMailer::Base.delivery_method = :smtp
