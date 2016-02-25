ce266d29bad934ff577ab9fe728ea3ad079107f1
Leezoon Khatua <leekhatua@gmail.com>
2016-02-25 22:42:42 +0000

    Again added sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -5,11 +5,12 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :port           => ENV['MAILGUN_SMTP_PORT'],
-  :address        => ENV['MAILGUN_SMTP_SERVER'],
-  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'http://secret-atoll-90009.herokuapp.com/',
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
   :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
 }
-ActionMailer::Base.delivery_method = :smtp
+
