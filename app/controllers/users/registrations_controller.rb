5246e90b31f45c053f0a580b524842a22d4b9a8e
Leezoon Khatua <leekhatua@gmail.com>
2016-03-08 22:58:06 +0000

    Changed new action in profiles controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -15,10 +15,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
     end
     
     private 
-        def select_plan
-         unless params[:plan] && (params[:plan] == '1' || params[:plan] == '2')
-         flash[:notice] = "Please select a membership plan to sign up."
-         redirect_to root_url
+    def select_plan
+        unless params[:plan] && (params[:plan] == '1' || params[:plan] == '2')
+        flash[:notice] = "Please select a membership plan to sign up."
+        redirect_to root_url
         end
     end
 end
